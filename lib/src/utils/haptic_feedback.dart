c7c8750b0bd0b065a5a831ff468835b22afc4744
@iamsannyrai <iam.sannyrai@gmail.com>
2021-07-18 11:23:59 +0545

    adds different vibration when cash recognitions is successfull or failure

---
+++ lib/src/utils/haptic_feedback.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_vibrate/flutter_vibrate.dart';
+
+class HapticFeedback{
+  static bool canVibrate;
+
+  /// makes sure whether vibration is supported or not
+  /// this function runs only once preventing method call from being
+  /// called again and again.
+  static Future<void> ensureInitialized() async{
+      canVibrate = await Vibrate.canVibrate;
+  }
+}
